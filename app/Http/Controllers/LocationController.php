9f0388ffe58297c996c57fc54bb0150b86b80612
Rainfog <muhammadzhafranbahij@gmail.com>
2024-06-24 08:06:47 +0700

    Feat: view location

---
+++ app/Http/Controllers/LocationController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Location;
+use App\Http\Requests\StoreLocationRequest;
+use App\Http\Requests\UpdateLocationRequest;
+use ProtoneMedia\Splade\SpladeTable;
+
+class LocationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $locations = SpladeTable::for(Location::class)
+                        ->column('id', sortable: true)
+                        ->column('name', sortable: true)
+                        ->column('created_at', sortable: true)
+                        ->column('updated_at', sortable: true)
+                        ->paginate(15);
+
+
+        return view('location.index', [
+            'locations' => $locations,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLocationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Location $location)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Location $location)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLocationRequest $request, Location $location)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Location $location)
+    {
+        //
+    }
+}
